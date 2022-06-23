9b3a149f37068a7c0e12f9f62b3826220073e11d
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-06-23 15:48:28 +0200

    first commit

---
+++ resources/js/components/AppMain.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{titolo}}</h1>
+        <h2>{{sottotitolo}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppMain',
+    data(){
+        return {
+            'titolo': 'Work In Progress',
+            'sottotitolo' : 'Sito in costruzione'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
